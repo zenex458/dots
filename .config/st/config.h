94b1f4b50e96815832fdcefa15351c03fd66746d
zenex <zenex@puffy.my.domain>
2022-08-10 19:57:09 +0100

    updated font fo nerd fonts firamono

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraMono-Medium:pixelsize=18:antialias=true:autohint=true";
+static char *font = "FiraMono Nerd Font Mono:Medium:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
