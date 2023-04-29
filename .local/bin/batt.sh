e7e412312da2479dd891ca07ef90dee780b1aa06
zenex <>
2023-04-29 15:23:27 +0100

    updated and moves scripts to local/bin

--- .local/bin/batt.sh
+++ .local/bin/batt.sh
@@ -6,7 +6,7 @@ batlvl="$(cat /sys/class/power_supply/BAT0/capacity)"
 batstatus="$(cat /sys/class/power_supply/BAT0/status)"
 if [ "$batlvl" -le 20 ] && [ "$batstatus" = "Discharging" ]
 then
-    notify-send -u critical "Battery low" "Battery level is ${batlvl}%!" && mpg123 --loop 2 ~/Downloads/yyt/other/Notification.mp3
+    notify-send -u critical "Battery low" "Battery level is ${batlvl}%!" #&& mpg123 --loop 2 ~/Downloads/yyt/other/Notification.mp3
 fi
 sleep 2m
 done &
