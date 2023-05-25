136641f4e4e4331e4ae4b97deadef72b1e7de26c
zenex <>
2023-05-25 21:34:18 +0100

    updated firefoxsandbox.nix

--- .local/bin/firefox-sandbox.nix
+++ .local/bin/firefox-sandbox.nix
@@ -1,35 +1,49 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
+
 let
-  firefoxSandbox = pkgs.bubblewrapSandbox {
-    home = "/home/firefox";
-    extraOptions = [
-      "--ro-bind=/home/user/Downloads /home/firefox/Downloads"
-      "--symlink=/run/user/1000/wayland-0"
-      "--ro-bind=/run/user/1000/wayland-0 /run/user/1000/wayland-0"
-      "--ro-bind=/run/user/1000/pipewire-0 /run/user/1000/pipewire-0"
-    ];
-  };
+  downloadsDir = "/home/<username>/Downloads";  # Replace <username> with your actual username
+
 in
 {
-  environment.systemPackages = with pkgs; [
-    (pkgs.firefox.override { preFixup = "bwrap --ro-bind /dev /dev --ro-bind /etc/fonts /etc/fonts --ro-bind /etc/machine-id /etc/machine-id --ro-bind /usr /usr --ro-bind /var /var --symlink /tmp /var/tmp --symlink /run /var/run --proc /proc --ro-bind /usr/lib/libgtk-3.so.0 /usr/lib/libgtk-3.so.0 --ro-bind /usr/share/themes /usr/share/themes"; postFixup = ""; }) 
-  ];
-  boot.extraKernelParams = ["systemd.unified_cgroup_hierarchy=1"];
-  system.activationScripts.bubblewrap = {
-    text = ''
-      #!/bin/sh
-      exec ${firefoxSandbox} "$@"
-    '';
-    requires = [ "bubblewrap" ];
+  options.services.firefox-sandbox = {
+    enable = mkEnableOption "Enable the Firefox sandbox";
+
+    user = config.users.users.<username>;
+
+    # Define the sandbox environment
+    # Note: Adjust the options based on your requirements
+    sandboxOptions = {
+      bindMounts = [
+        # Only allow access to the downloads directory
+        { hostPath = downloadsDir; targetPath = "/home/firefox/Downloads"; }
+      ];
+
+      extraOptions = [
+        "--unshare-net"  # Unshare network namespace
+        "--unshare-ipc"  # Unshare IPC namespace
+      ];
+    };
   };
-  systemd.user.services.firefox-sandbox = {
-    description = "Firefox sandbox";
-    wantedBy = [ "graphical-session.target" ];
-    serviceConfig.Type = "forking";
-    serviceConfig.ExecStart = "${pkgs.stdenv.shell} -c 'bwrap ${pkgs.stdenv.shell} -c ${firefoxSandbox}'";
-    serviceConfig.Restart = "on-failure";
-    serviceConfig.RestartSec = "2";
-    wantedBy = [ "graphical-session.target" ];
+
+  config = lib.mkIf config.services.firefox-sandbox.enable {
+    security.bubblewrap.sandboxWrapper = {
+      firefox = {
+        exec = pkgs.firefox-bin;
+        user = "firefox";
+        args = [
+          "--new-instance"              # Run in a new instance
+          "--no-remote"                 # Don't connect to an existing Firefox instance
+          "--class=Firefox-sandboxed"   # Set a unique class name for the sandboxed Firefox window
+        ];
+      };
+    };
+
+    users.users.<username>.services = {
+      firefox-sandbox = {
+        enable = true;
+        sandboxOptions = config.services.firefox-sandbox.sandboxOptions;
+      };
+    };
   };
 }
 
