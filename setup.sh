6ce1b9b0c3dd8f30f0f12b5ef67c0e02dec7a1f6
zenex <>
2023-06-02 00:05:07 +0100

    added swayidle

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ postsetup ()
 	read -rp "Do you want sway?(y/n) " sw
 	if [[ $sw == "y" || $sw == "Y" ]]
 	then
-            sudo $pkg sway foot waybar gammastep xwayland bemenu swaylock
+            sudo $pkg sway foot waybar gammastep xwayland bemenu swaylock swayidle
 	else
 	  echo "sway not added"
 	fi
