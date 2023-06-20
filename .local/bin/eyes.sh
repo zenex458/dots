6d2721e88101c42410698301144b924ebf30fbc7
zenex <>
2023-06-21 00:16:07 +0100

    removed unneeded scripts and updated eyes.sh

--- .local/bin/eyes.sh
+++ .local/bin/eyes.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env dash
 
-case $XDG_SESSION_TYPE in
-	"wayland")  shift="gammastep -P -o -O 2000" && norm="gammastep -o";;
-	*)  shift="redshift -x && redshift -o -O 2000" && norm="redshift -o";;
-esac 
+##case $XDG_SESSION_TYPE in
+##	"wayland")  shift="gammastep -P -o -O 2000"
+##				norm="gammastep -o"
+##				;;
+##	*)  shift="redshift -x && redshift -o -O 2000"
+##		norm="redshift -o"
+##		;;
+##esac 
+##
+##while true; do
+##    hour="$(date +%H)"
+##    if [ "$hour" -le 7 ] || [ "$hour" -ge 16 ]; then
+##		$shift
+##    else 
+##	    $norm
+##    fi
+##    sleep 40m
+##done &
+
+
 
 while true; do
     hour="$(date +%H)"
     if [ "$hour" -ge 16 ] || [ "$hour" -le 7 ]
     then
-	    $shift
+	    redshift -x && redshift -o -O 2000
     else
-	    $norm
+        redshift -x && redshift -o -c ~/.config/redshift.conf
     fi
     sleep 40m
 done &
-
-
-
-#while true; do
-#    hour="$(date +%H)"
-#    if [ "$hour" -ge 16 ] || [ "$hour" -le 7 ]
-#    then
-#	    redshift -x && redshift -o -O 2000
-#    else
-#        redshift -x && redshift -o -c ~/.config/redshift.conf
-#    fi
-#    sleep 40m
-#done &
