98d762c21978a57fff4cdf0bd1ce2e0df432c2c9
zenex <>
2023-06-21 00:13:34 +0100

    updated xmonad and xmobar config

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -16,6 +16,8 @@ import XMonad.Util.NamedScratchpad
 import XMonad.Util.SpawnOnce
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.IndependentScreens
+import XMonad.Layout.Tabbed
+import XMonad.Layout.SimpleFloat
 
 main :: IO ()
 main = xmonad 
@@ -28,9 +30,9 @@ myConfig = def
     { modMask    = mod4Mask
     , layoutHook = myLayout
     , normalBorderColor = "#000000"
-    , focusedBorderColor = "#ff0000"
+    , focusedBorderColor = "#999999"
 --    , startupHook = myStartupHook
-    , workspaces = myWorkspaces
+--    , workspaces = myWorkspaces
     , manageHook = myManageHook
     , keys = myKeys
     -- , manageHook = namedScratchpadManageHook scratchpads                       
@@ -46,11 +48,11 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
       -- private firefox
       ((mod1Mask, xK_c), spawn "firefox -P priv"),
       -- next song
-      ((mod1Mask, xK_o), spawn "mpc -p 6601 next"),
+      ((mod1Mask, xK_o), spawn "mpc next"),
       -- previous song
-      ((mod1Mask, xK_i), spawn "mpc -p 6601 prev"),
+      ((mod1Mask, xK_i), spawn "mpc prev"),
       -- play/pause song
-      ((mod1Mask, xK_p), spawn "mpc -p 6601 toggle"),
+      ((mod1Mask, xK_p), spawn "mpc toggle"),
       -- mute/unmute
       ((mod1Mask, xK_m), spawn "amixer sset Master toggle"),
       -- minus vol
@@ -61,6 +63,8 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
       ((mod1Mask .|. controlMask, xK_l), spawn "Menu"),
       -- spawn alsamixer
       ((modm, xK_a), spawn "urxvt -e alsamixer -V all"),
+      --spawn emacs without the site-file
+      ((modm, xK_u), spawn "emacs"),
       -- brightness up by 2
       ((mod1Mask, xK_Prior), spawn "light -A 2"),
       -- brightness down by 2
@@ -69,13 +73,15 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
       ((modm .|. shiftMask, xK_r), spawn "redshift -o -c ~/.config/redshift.conf"),
       -- application launcher
       ((modm, xK_p), spawn "rofi -show combi -modes combi -combi-modes 'window,run'"),
-      ((modm, xK_m), namedScratchpadAction scratchpads "music"),
+--      ((modm, xK_m), namedScratchpadAction scratchpads "music"),
       -- close focused window
       ((modm, xK_q), kill),
       -- Rotate through the available layout algorithms
       ((modm, xK_space), sendMessage NextLayout),
-     -- ((modm, xK_m), sendMessage $ JumpToLayout "Full"), 
-     -- ((modm, xK_t), sendMessage $ JumpToLayout "ResizeableTall"),
+      ((modm, xK_m), sendMessage $ JumpToLayout "Full"), 
+      ((modm, xK_t), sendMessage $ JumpToLayout "ResizableTall"),
+      ((mod1Mask, xK_t), sendMessage $ JumpToLayout "Tabbed Simplest"),
+      ((mod1Mask, xK_f), sendMessage $ JumpToLayout "Simple Float"),
       --  Reset the layouts on the current workspace to default
       ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf),
       -- Resize viewed windows to the correct size
@@ -114,17 +120,17 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
       ++
       -- mod-[1..9], Switch to workspace N
       -- mod-shift-[1..9], Move client to workspace N
---normal
- --     [ ((m .|. modm, k), windows $ f i)
- --       | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9],
- --         (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
- --     ]
+-- normal
+      [ ((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9],
+          (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+      ]
 
 
-        [ ((m .|. modm, k), windows $ onCurrentScreen f i)
-          | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9],
-            (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-        ]
+  --      [ ((m .|. modm, k), windows $ onCurrentScreen f i)
+  --        | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9],
+  --          (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+  --      ]
 
 
 
@@ -136,7 +142,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
         | (key, sc) <- zip [xK_w, xK_e, xK_z] [0 ..],
           (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
       ]
-myLayout = lessBorders (Screen) tiled ||| noBorders Full
+myLayout = lessBorders (Screen) tiled ||| noBorders Full ||| noBorders simpleTabbed ||| simpleFloat
   where
     tiled    = ResizableTall nmaster delta ratio []
     nmaster  = 1
@@ -167,57 +173,63 @@ myManageHook = composeAll
   -- I'm doing it this way because otherwise I would have to write out the full
   -- name of my workspaces and the names would be very long if using clickable workspaces.
   [ title =? "alsamixer" --> doCenterFloat
-  ] <+> namedScratchpadManageHook scratchpads
+    ,appName =? "Browser" --> doCenterFloat
+  ] -- <+> namedScratchpadManageHook scratchpads
 
-
-
-
-scratchpads = [
-    NS "music" "urxvtc -e ncmpcpp" (title =? "ncmpcpp")
-        (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
-
-              ] where role = stringProperty "WM_WINDOW_ROLE"
-
---myXmobarPP :: PP
---myXmobarPP = def
---    { ppCurrent = xmobarColor "#c6c6c6" "" . wrap "[" "]"
---      , ppTitle = xmobarColor "#c6c6c6" ""
---      , ppSep = " "
---      , ppUrgent = xmobarColor "#ff0000" "" . wrap "!" "!"
---      , ppOrder = \(ws : l : t : ex) -> [ws, l] ++ ex ++ [t]
---      , ppLayout =
---        ( \x -> case x of
---            "Tall" -> "" --[]=
---            "Full" -> "" --[]
---        )
---    }
+--scratchpads = [
+--    NS "music" "urxvtc -e ncmpcpp" (title =? "ncmpcpp")
+--        (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
+--
+--              ] where role = stringProperty "WM_WINDOW_ROLE"
 
 myXmobarPP :: PP
 myXmobarPP = def
-    { ppSep             = " "
-    , ppTitleSanitize   = xmobarStrip
-    , ppCurrent 	= white . wrap "[" "]"
-    , ppVisible         = wrap "(" ")"
-    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
-    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
-    , ppExtras          = [logTitles formatFocused formatUnfocused]
-    , ppLayout =
+    { ppCurrent = xmobarColor "#c6c6c6" "" . wrap "[" "]"
+      , ppTitle = xmobarColor "#c6c6c6" ""
+      , ppVisible = wrap "(" ")"
+      , ppSep = " "
+      , ppUrgent = xmobarColor "#ff0000" "" . wrap "!" "!"
+      , ppOrder = \(ws : l : t : ex) -> [ws, l] ++ ex ++ [t]
+          , ppLayout =
         ( \x -> case x of 
-        "Tall" -> "" --[]= 
-        "ResizableTall" -> "" --[]= 
-        "Full" -> "" --[]
+        "Tall" -> "[]=="
+        "ResizableTall" -> "[]=" 
+        "Full" -> "[]"
+        "Simple Float" -> "^"
+        "Tabbed Simplest" -> "--"
         _ -> "LAYOUT NOT DETECTED"
         )
+
     }
-  where
-    formatFocused   = wrap (white    "[") (white    "]") . white  .  ppWindow
-    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . lowWhite  . ppWindow
 
-    ppWindow :: String -> String
-    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 70
-
-    lowWhite, red, white, yellow :: String -> String
-    white    = xmobarColor "#f8f8f2" ""
-    yellow   = xmobarColor "#ffff00" ""
-    red      = xmobarColor "#ff0000" ""
-    lowWhite = xmobarColor "#bbbbbb" ""
+--myXmobarPP :: PP
+--myXmobarPP = def
+--    { ppSep             = " "
+--    , ppTitleSanitize   = xmobarStrip
+--    , ppCurrent         = white . wrap "[" "]"
+--    , ppVisible         = wrap "(" ")"
+--    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+--    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+--    , ppExtras          = [logTitles formatFocused formatUnfocused]
+--    , ppLayout =
+--        ( \x -> case x of 
+--        "Tall" -> "[]=="
+--        "ResizableTall" -> "[]=" 
+--        "Full" -> "[]"
+--        "Simple Float" -> "^"
+--        "Tabbed Simplest" -> "--"
+--        _ -> "LAYOUT NOT DETECTED"
+--        )
+--    }
+--  where
+--    formatFocused   = wrap (white    "[") (white    "]") . white  .  ppWindow
+--    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . lowWhite  . ppWindow
+--
+--    ppWindow :: String -> String
+--    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 70
+--
+--    lowWhite, red, white, yellow :: String -> String
+--    white    = xmobarColor "#f8f8f2" ""
+--    yellow   = xmobarColor "#ffff00" ""
+--    red      = xmobarColor "#ff0000" ""
+--    lowWhite = xmobarColor "#bbbbbb" ""
