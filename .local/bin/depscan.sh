9a1865efe4bd6a54ac724059cad07f87536ce574
zenex <>
2024-01-08 15:37:52 +0000

    Updated .local/bin directory with some new scripts as well ass updating some of the old ones.

---
+++ .local/bin/depscan.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+sudo clamscan -irv --exclude=/proc --exclude=/sys --exclude=/dev --exclude=/media --exclude=/mnt
--- .local/bin/status.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-while true; do 
-	xsetroot -name "$(cat /sys/class/power_supply/BAT0/capacity)$(echo "%") $(date +"%a %d/%m %T" )"
-   sleep 1
-done &
-
