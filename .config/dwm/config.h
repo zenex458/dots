388886dd605c3d313beaf84de57095efe4e83967
zenex458 <emajeed458@gmail.com>
2024-09-30 21:55:26 +0100

    updated dwm font

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Monospace Bold:size=10" };
-static const char dmenufont[]       = "Monospace Bold:size=10";
+static const char *fonts[]          = { "FiraMono NF:Bold:size=10" };
+static const char dmenufont[]       = "FiraMono NF:Bold:size=10";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
