61522afe850aecfb18d7ac547dfac6d089b3bd44
zenex458 <emajeed458@gmail.com>
2024-09-30 21:55:29 +0100

    updated some confs

--- .config/status.sh
+++ .config/status.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 while true; do 
-	xsetroot -name "$(cat /sys/class/power_supply/BAT0/capacity)$(echo "%") $(date +"%d/%m %T" )"
+	xsetroot -name "$(cat /sys/class/power_supply/BAT0/capacity)$(echo "%") $(date +"%a %d/%m %T" )"
    sleep 1
 done &
 
