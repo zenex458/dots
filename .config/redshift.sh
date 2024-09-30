71f28fa4e9660947b21c98fc8e536ae6b685396a
zenex458 <emajeed458@gmail.com>
2024-09-30 21:55:29 +0100

    changed some scripts to dash

--- .config/redshift.sh
+++ .config/redshift.sh
@@ -1,15 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env dash
 while true; do
-hour="`date +%H`"
-case $hour in
-	7)  redshift -x;;
-	16) redshift -x && redshift -o -O 4500;;
-	20) redshift -x && redshift -o -O 3000;;
-	21) redshift -x && redshift -o -O 2000;;
-	22) redshift -x && redshift -o -O 2000;;
-	23) redshift -x && redshift -o -O 2000;;
-	00) redshift -x && redshift -o -O 2000;;
-	*)  redshift -x && redshift -o -c ~/.config/redshift.conf;;
-esac
-sleep 60m
+hour="$(date +%H)"
+if [ "$hour" -ge 16 ] || [ "$hour" -le 7 ]
+then
+	redshift -x && redshift -o -O 2000
+fi
+sleep 40m
 done &
