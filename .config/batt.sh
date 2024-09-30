7b331864b6fd924f598493ccdbcee6ec5767f8db
zenex458 <emajeed458@gmail.com>
2024-09-30 21:55:28 +0100

    updated some scripts and nixos conf

--- .config/batt.sh
+++ .config/batt.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 while true; do
 
 #batlvl="`acpi -b | grep -P -o '[0-9]+(?=%)'`"
 batlvl="`cat /sys/class/power_supply/BAT0/capacity`"
 if [[ $batlvl -le 15 ]]
 then
-    notify-send -u critical "Battery low" "Battery level is ${batlvl}%!"
+    notify-send -u critical "Battery low" "Battery level is ${batlvl}%!" && mpv ~/Downloads/yyt/other/Notification.mp3 && mpv ~/Downloads/yyt/other/Notification.mp3
 fi
 sleep 2m
 done &
--- .config/status.sh
+++ .config/status.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-while true; do                                                                   xsetroot -name "$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master)) $(cat /sys/class/power_supply/BAT0/capacity) $(date +"%d/%m %T" )"
+#!/usr/bin/env bash
+while true; do 
+	xsetroot -name "$(cat /sys/class/power_supply/BAT0/capacity)$(echo "%") $(date +"%d/%m %T" )"
    sleep 1
 done &
 
