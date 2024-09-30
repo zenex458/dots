ff44b3f65c8bf4a865a4a80a6957534999528735
zenex458 <emajeed458@gmail.com>
2024-09-30 21:55:28 +0100

    updated batt.sh and redshift.sh

--- .config/batt.sh
+++ .config/batt.sh
@@ -3,7 +3,9 @@ while true; do
 
 #batlvl="`acpi -b | grep -P -o '[0-9]+(?=%)'`"
 batlvl="`cat /sys/class/power_supply/BAT0/capacity`"
-if [[ $batlvl -le 20 ]]
+batstatus="`cat /sys/class/power_supply/BAT0/status`"
+
+if [[ $batlvl -le 20 && $batstatus == "Discharging" ]]
 then
     notify-send -u critical "Battery low" "Battery level is ${batlvl}%!" && mpg123 --loop 2 ~/Downloads/yyt/other/Notification.mp3
 fi
