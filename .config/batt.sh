dc97fd4f4b2aebcc022efce58e7a7fb44992fb1d
zenex458 <emajeed458@gmail.com>
2024-09-30 21:55:27 +0100

    updated setup script and moved to catting instead of using acpi

--- .config/batt.sh
+++ .config/batt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 while true; do
 
-batlvl="`acpi -b | grep -P -o '[0-9]+(?=%)'`"
+#batlvl="`acpi -b | grep -P -o '[0-9]+(?=%)'`"
+batlvl="`cat /sys/class/power_supply/BAT0/capacity`"
 if [[ $batlvl -le 15 ]]
 then
     notify-send -u critical "Battery low" "Battery level is ${batlvl}%!"
