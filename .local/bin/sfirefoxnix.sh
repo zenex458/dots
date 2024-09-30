4b177fb38dac30ec7330b53ade433bbc71c01206
zenex458 <emajeed458@gmail.com>
2024-09-30 21:55:33 +0100

    updated firefoxnis

--- .local/bin/sfirefoxnix.sh
+++ .local/bin/sfirefoxnix.sh
@@ -1,20 +1,26 @@
-#!/bin/sh
-exec bubblewrap \
-	 --unshare-pid \
-	 --unshare-net \
-	 --unshare-ipc \
-	 --ro-bind /etc /etc \
-	 --ro-bind /usr /usr \
-	 --ro-bind /var /var \
-	 --new-session \
-	 --chdir=$HOME \
-	 --dir=$HOME/Downloads \
-	 --bind=$HOME/Downloads $HOME/Downloads \
-	 --bind=/dev /dev \
-	 --bind=/tmp /tmp \
-	 --bind=/run /run \
-	 --bind=/etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt \
-	 --bind=/etc/machine-id /etc/machine-id \
-	 --bind=/run/user/$(id -u)/pipewire-0 /run/user/$(id -u)/pipewire-0 \    # Add this line for Pipewire access
---bind=/run/user/$(id -u)/wayland-0 /run/user/$(id -u)/wayland-0 \    # Add this line for Wayland access
---file 10 firefox -no-remote -profile $HOME/.firefox-profile
+#!/usr/bin/env sh
+exec bwrap --unshare-all --ro-bind /usr /usr \
+     --ro-bind /etc /etc \
+     --ro-bind /bin /bin \
+     --ro-bind /lib /lib \
+     --ro-bind /lib64 /lib64 \
+     --tmpfs /tmp \
+     --symlink usr/lib /lib \
+     --symlink usr/lib64 /lib64 \
+     --dev /dev \
+     --dev-bind /dev/shm /dev/shm \
+     --dev-bind /dev/pts /dev/pts \
+     --proc /proc \
+     --dir /home/firefox-sandboxed/Downloads /home/firefox-sandboxed/Downloads \
+     --unshare-ipc --unshare-pid --unshare-uts \
+     --unshare-net \
+     --unshare-user \
+     --unshare-cgroup \
+     --ro-bind /var/run/dbus /var/run/dbus \
+     --unshare-ipc \
+     --unshare-pid \
+     --unshare-uts \
+     --unshare-cgroup \
+     --ro-bind /var/run/dbus /var/run/dbus \
+     --chdir / \
+     /usr/bin/firefox --no-remote
