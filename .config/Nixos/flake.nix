e61808a0142a64c1396518b1fc6f06b990a70969
zenex458 <emajeed458@gmail.com>
2025-01-05 15:32:06 +0000

    Updated flake.nix

--- .config/Nixos/flake.nix
+++ .config/Nixos/flake.nix
@@ -1,12 +1,7 @@
 {
-  description = "NixOS configuration";
+  description = "Nix config";
 
   inputs = {
-<<<<<<< HEAD
-    nixpkgs.url = "nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-=======
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     #    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -14,61 +9,31 @@
     # https://librephoenix.com/2024-02-10-using-both-stable-and-unstable-packages-on-nixos-at-the-same-time
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.11";
->>>>>>> main
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-<<<<<<< HEAD
-      nixpkgs-unstable,
-      home-manager,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-      overlay-unstable = final: prev: {
-        unstable = import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = ["x86_64-linux"];
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      eukaryotic = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs outputs;
         };
-
-      };
-    in
-    {
-      nixosConfigurations = {
-        eukaryotic = nixpkgs.lib.nixosSystem {
-          inherit system;
-=======
-      home-manager,
-      ...
-    }@inputs:
-    let
-      inherit (self) outputs;
-      # Supported systems for your flake packages, shell, etc.
-      systems = [ "x86_64-linux" ];
-    in
-    {
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild --flake .#your-hostname'
-      nixosConfigurations = {
-        eukaryotic = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs;
-          };
->>>>>>> main
-          modules = [
-            (
-              { config, pkgs, ... }:
-              {
-                nixpkgs.overlays = [ overlay-unstable ];
-              }
-            )
-            ./nixos/configuration.nix
-          ];
-        };
+        modules = [
+          # > Our main nixos configuration file <
+          ./nixos/configuration.nix
+        ];
       };
     };
+  };
 }
