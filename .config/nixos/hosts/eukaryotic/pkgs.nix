cf6859aacea3b67c79a76adad60b764a0f7678d2
zenex458 <emajeed@protonmail.com>
2025-05-22 20:01:38 +0100

    NIXOS: Removed duplication of options and moved them to pkgs.nix

---
+++ .config/nixos/hosts/eukaryotic/pkgs.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  nixpkgs = {
+    # Configure your nixpkgs instance
+    config = {
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+    overlays = [
+      (final: prev: {
+        unstable = import inputs.nixpkgs-unstable {
+          inherit prev;
+          system = prev.system;
+          config.allowUnfree = true;
+        };
+        keepassxc = prev.keepassxc.override {
+          withKeePassBrowserPasskeys = false;
+          withKeePassBrowser = false;
+          withKeePassNetworking = false;
+          withKeePassKeeShare = false;
+          withKeePassSSHAgent = false;
+        };
+      })
+    ];
+  };
+}
