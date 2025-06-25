aaab664562984772d722d0ae3599195fc6f49d2e
zenex458 <emajeed@protonmail.com>
2025-06-25 15:42:21 +0100

    NIXOS: Added keepassxc compile options

--- .config/nixos/hosts/eukaryotic/pkgs.nix
+++ .config/nixos/hosts/eukaryotic/pkgs.nix
@@ -20,6 +20,7 @@
           system = prev.system;
           config.allowUnfree = true;
         };
+        #options are here: https://github.com/NixOS/nixpkgs/blob/c7ab75210cb8cb16ddd8f290755d9558edde7ee1/pkgs/by-name/ke/keepassxc/package.nix#L26
         keepassxc = prev.keepassxc.override {
           withKeePassBrowserPasskeys = false;
           withKeePassBrowser = false;
