a150102d8479cf585cde7ebdd1bcd43d66b4baba
zenex458 <emajeed@protonmail.com>
2025-08-02 22:43:18 +0100

    Quoted expressions renamed Mute to Toggle, shows popup if too loud

--- .local/bin/vol.sh
+++ .local/bin/vol.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-userarg=$(printf "10\n15\n20\n25\n30\n35\nOther\nMute" | bemenu -p Volume\ Level\ \($(amixer get Master | grep -o [0-9]*% | head -n 1)\):)
+userarg=$(printf "10\n15\n20\n25\n30\n35\nOther\nToggle" | bemenu -p Volume\ Level\ \("$(amixer get Master | grep -o "[0-9]*%" | head -n 1)"\):)
 
 if [ "$userarg" -gt 35 ]; then
-    echo no
+    printf "Too loud" | bemenu
 elif [ "$userarg" = "Other" ]; then
-	userarg=$(printf "" | bemenu -p Custom\ Volume\ Level\ \($(amixer get Master | grep -o [0-9]*% | head -n 1)\):)
+	  userarg=$(printf "" | bemenu -p Custom\ Volume\ Level\ \("$(amixer get Master | grep -o "[0-9]*%" | head -n 1)"\):)
     amixer sset Master "$userarg"%
 
-elif [ "$userarg" = "Mute" ]; then
+elif [ "$userarg" = "Toggle" ]; then
     amixer sset Master toggle
-    else
-        amixer sset Master "$userarg"%
+else
+    amixer sset Master "$userarg"%
 fi
 
 
