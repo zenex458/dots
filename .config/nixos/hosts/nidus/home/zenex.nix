ea78601c7dd47c5a1057ba5152ed9ebac527815c
zenex458 <emajeed@protonmail.com>
2025-09-23 14:56:06 +0100

    Removed rounded corners for gtk

--- .config/nixos/hosts/nidus/home/zenex.nix
+++ .config/nixos/hosts/nidus/home/zenex.nix
@@ -62,10 +62,6 @@
       hyprlock.enable = false;
       tmux.enable = false;
       #  neovim.enable = false;
-      gtk.extraCss = ''
-        // Remove rounded corners
-        window.background { border-radius: 0; }
-      '';
     };
 
     base16Scheme = {
