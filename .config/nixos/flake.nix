1b440f077f428d0b9e0721bf98bbbecf1c8a23b7
zenex458 <emajeed@protonmail.com>
2025-11-02 15:30:51 +0000

    feat: optimised flake.nix so now it nixosConfiguations has a map to the hostnames

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A template that shows all standard flake outputs";
+  description = "My flake";
 
   # Inputs
   # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
@@ -7,54 +7,49 @@
   # It is also possible to "inherit" an input from another input. This is useful to minimize
   # flake dependencies. For example, the following sets the nixpkgs input of the top-level flake
   # to be equal to the nixpkgs input of the nixops input of the top-level flake:
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-    lanzaboote.url = "github:nix-community/lanzaboote/v0.4.2";
-    lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:nix-community/stylix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     impermanence.url = "github:nix-community/impermanence";
-    stylix.url = "github:nix-community/stylix/release-25.05";
-    niri.url = "github:sodiboo/niri-flake";
-    niri.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   # For more information about well-known outputs checked by `nix flake check`:
   # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake-check.html#evaluation-checks
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    disko,
-    impermanence,
-    lanzaboote,
-    stylix,
-    nixpkgs-unstable,
-    niri,
-    ...
-  } @ inputs: {
-    # Used with `nixos-rebuild --flake .#<hostname>`
-    # nixosConfigurations."<hostname>".config.system.build.toplevel must be a derivation
-    nixosConfigurations = {
-      nidus = nixpkgs.lib.nixosSystem {
+  outputs = inputs @ {nixpkgs, ...}: let
+    # Used with `nixos-rebuild --flake .#<hosts>`
+    # nixosConfigurations."<hosts>".config.system.build.toplevel must be a derivation
+    hosts = ["nidus" "tetatnus"];
+    mkSystem = hostname:
+      nixpkgs.lib.nixosSystem {
         specialArgs = {
           inherit inputs;
         };
         system = "x86_64-linux";
-        modules = [./hosts/nidus/configuration.nix];
+        modules = [./hosts/${hostname}/configuration.nix];
       };
-
-      tetanus = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs;
-        };
-        system = "x86_64-linux";
-        modules = [./hosts/tetanus/configuration.nix];
-      };
-    };
+  in {
+    nixosConfigurations = nixpkgs.lib.genAttrs hosts mkSystem;
   };
 }
