d1ff21020008d092f305741b01bae6caff259760
zenex458 <emajeed@protonmail.com>
2025-11-08 13:58:43 +0000

    fix: ttyp0 overlay not setting the build options correctly

--- .config/nixos/overlays/ttyp0.nix
+++ .config/nixos/overlays/ttyp0.nix
@@ -3,16 +3,7 @@
     overlays = [
       (final: prev: {
         uw-ttyp0 = prev.uw-ttyp0.override {
-          variantsDat = ''
-
-            cat << EOF > VARIANTS.dat
-            COPYTO AccStress PApostropheAscii
-            COPYTO PAmComma AccGraveAscii
-            COPYTO Digit0Slashed Digit0
-            COPYTO MTilde AccTildeAscii
-            COPYTO Space SpaceNoBreak
-            EOF
-          '';
+          variantsDat = ./variants.dat;
         };
       })
     ];
