a1bcd2626c2f8933245260a019cd3072f5ecb300
zenex458 <emajeed@protonmail.com>
2025-11-21 19:41:40 +0000

    fix: disabled coloured manpages as they didn't work. Sorted file whitelist, added nix-index to persistence

--- .config/nixos/home/default.nix
+++ .config/nixos/home/default.nix
@@ -100,7 +100,6 @@
           XCURSOR_SIZE = 20;
           BEMENU_OPTS = ''-i --fn 'Iosevka' -B '1' -f -p '>' -n --tb '#bdae93' --tf '#060606' --fb '#060606' --ff '#bdae93' --nb '#060606' --nf '#bdae93' --ab '#060606' --af '#bdae93' --sb '#060606' --sf '#bdae93' --cb '#bdae93' --cf '#bdae93' --hb '#bdae93' --hf '#060606' --sb '#bdae93' --sf '#060606' --scb '#060606' --scf '#bdae93' --bdr '#bdae93' '';
           # MATHPATH = "/run/current-system/sw/share/man";
-          MANPAGER = "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat -p -lman'";
         };
         file = {
           ".local/bin" = {
@@ -118,24 +117,24 @@
             ".config/emacs"
             ".config/feather"
             ".config/gh"
+            ".config/netbird"
             ".config/opensnitch"
             ".config/Signal"
             ".config/simplex"
             ".config/vesktop"
             ".config/zotero"
             ".config/zsh"
-            ".config/netbird"
+            ".icons"
+            ".local/share/fish"
             ".local/share/simplex"
             ".local/state/wireplumber"
             ".mozilla"
-            ".icons"
             "Dev"
             "Documents"
             "Downloads"
             "Music"
-            ".local/share/fish"
           ];
-          files = [".local/share/.bash_history"];
+          files = [".local/share/.bash_history" ".cache/nix-index/files"];
           allowOther = true;
         };
         sessionPath = [
