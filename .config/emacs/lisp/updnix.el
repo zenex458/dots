3ee6da22e4557e75b09b0701099b8d5c68c9bff5
zenex458 <emajeed@protonmail.com>
2026-01-28 13:51:27 +0000

    fix: updnix() now uses the updated --sudo flag for building

--- .config/emacs/lisp/updnix.el
+++ .config/emacs/lisp/updnix.el
@@ -19,7 +19,7 @@
 (defun updnix ()
   "Update nixos."
   (interactive)
-  (zenex/sudo-shell-command "nixos-rebuild --use-remote-sudo switch --flake /home/zenex/Dev/dots/.config/nixos/#nidus"))
+  (zenex/sudo-shell-command "nixos-rebuild --sudo switch --flake /home/zenex/Dev/dots/.config/nixos/#nidus"))
 
 (provide 'updnix)
 ;;; updnix.el ends here
