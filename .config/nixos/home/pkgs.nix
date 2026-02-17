df4dae9c99446c1ed696ad7aeb2cf7f59adbcb8f
zenex458 <emajeed@protonmail.com>
2026-02-17 10:11:04 +0000

    feat: added new packages

--- .config/nixos/home/pkgs.nix
+++ .config/nixos/home/pkgs.nix
@@ -15,125 +15,136 @@
     # pynitrokey
     # rlwrap # for the readline
     # sigrok-cli
-    # wl-color-picker
     # yewtube
     age
-    anki
     alejandra
     alsa-utils
     amdgpu_top
+    anki
     aria2
-    nautilus
     bc
     bemenu
     bfs
+    bibtex-tidy
+    binwalk
+    burpsuite
     cliphist
+    cmake-format
     cryptsetup
+    cutter
     dig
     exfatprogs
     exif
     feather
     ffmpeg-full
     ffmpegthumbnailer
+    ffuf
     file
     fuse3
     fzy
+    gcc
+    gdb
+    gef
     gh
+    ghidra-extensions.findcrypt
+    ghidra-extensions.ghidra-golanganalyzerextension
+    ghidra-extensions.ghidraninja-ghidra-scripts
     gimp3-with-plugins
     grc
     grim
+    hashcat
     html-tidy
     htop
+    hujsonfmt
     hunspell
     hunspellDicts.en-gb-large
     imagemagick
     imv
-    jmtpfs
+    inetutils
     irssi
+    jetbrains.clion
+    jmtpfs
+    john
     jq
     libnotify
     libreoffice
     lsof
     magic-wormhole
     man-pages
-    wakeonlan
     man-pages-posix
     moreutils
     mpc
+    multimon-ng
     mupdf
+    nautilus
     nemo-with-extensions
+    netcat-gnu
     nitrokey-app2
     nixd
+    nmap
+    nmap
     nodePackages.bash-language-server
     nodePackages_latest.prettier
+    openvpn
     p7zip
     pandoc
     pciutils
     pcsc-tools
+    picard
+    profanity
     pulsemixer
+    python313
+    python313Packages.numpy
+    python313Packages.requests
+    python313Packages.z3-solver
     restic
     ripgrep
     ripgrep-all
     rsync
     samba4Full
     sbctl
+    seclists
     shellcheck
     shfmt
-    unstable.signal-desktop
     slurp
+    sonic-visualiser
+    spek
+    steghide
+    stegseek
     swaybg
     syncthing
     tarsnap
-    typst
-    typstyle
-    typst-live
+    tenacity
     texlab
     texliveFull
-    bibtex-tidy
+    tinymist
     traceroute
     trash-cli
     tree
+    typst
+    typst-live
+    typstyle
+    unstable.signal-desktop
     unstable.simplex-chat-desktop
     unzip
     usbutils
     vesktop
     virt-viewer
+    wakeonlan
     wdisplays
     wl-clip-persist
     wl-clipboard
+    wl-color-picker
     wlr-randr
     wlsunset
     xdg-utils
     xmlformat
-    tinymist
     xwayland-satellite
     yamlfmt
     yt-dlp
     zip
-    hujsonfmt
     zotero
-    seclists
-    nmap
-    cutter
-    jetbrains.clion
-    cmake-format
-    wl-color-picker
-    picard
-    profanity
-    inetutils
-    netcat-gnu
-    nmap
-    ffuf
-    python313
-    burpsuite
-    openvpn
-    binwalk
     (pkgs.wordlists.override {lists = with pkgs; [rockyou seclists];})
-    john
-    python313Packages.dirsearch
-    hashcat
-    gdb
-    gef
     (aspellWithDicts (
       dicts:
         with dicts; [
